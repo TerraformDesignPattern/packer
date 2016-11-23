730bb194a49cacfcb61c51738d586af4d42cf031
Jonathon Brouse <jonathon.brouse@spartasystems.com>
2016-11-23 21:28:08 +0000

    Fixed spacing in outputs.tf

--- terraform-ami-module/outputs.tf
+++ terraform-ami-module/outputs.tf
@@ -1,7 +1,7 @@
 output "docker_image_id" {
-    value = "${lookup(var.docker_image_ids, var.aws_region)}"
+  value = "${lookup(var.docker_image_ids, var.aws_region)}"
 }
 
 output "base_image_id" {
-    value = "${lookup(var.base_image_ids, var.aws_region)}"
+  value = "${lookup(var.base_image_ids, var.aws_region)}"
 }
