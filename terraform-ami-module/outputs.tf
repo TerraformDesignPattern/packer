4ddd5d059c6f501c0a0bfb56b7ceec3c2515dcc0
Jonathon Brouse <jonathon.brouse@spartasystems.com>
2016-11-23 19:48:57 +0000

    Added base_ami_image to outputs.tf

--- terraform-ami-module/outputs.tf
+++ terraform-ami-module/outputs.tf
@@ -1,3 +1,7 @@
 output "docker_image_id" {
     value = "${lookup(var.docker_image_ids, var.aws_region)}"
 }
+
+output "base_image_id" {
+    value = "${lookup(var.base_image_ids, var.aws_region)}"
+}
