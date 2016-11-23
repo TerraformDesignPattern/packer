df6da7d97d9c777f212be158bb68447b847f4305
Jonathon Brouse <jonathon.brouse@spartasystems.com>
2016-11-23 21:27:40 +0000

    Added docker-compose to build_ami.sh

--- build_ami.sh
+++ build_ami.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+# Packer build script to install Docker and Docker-Compose
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates git unzip
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-get update
+apt-get install -y apt-transport-https ca-certificates git unzip
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
 
-sudo apt-get update
-sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get install -y docker-engine
+apt-get update
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install -y docker-engine
+curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 #### Sanatize AMI ####
 rm -rf /var/lib/cloud/instances/
